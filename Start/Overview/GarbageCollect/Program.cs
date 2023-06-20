e32fe4ad0dc6a62689fc09814c7fd23aefaabf07
Dharani Vora <dharanivora@outlook.com>
2023-06-20 14:13:29 -0400

    chore: complete GarbageCollect

--- Start/Overview/GarbageCollect/Program.cs
+++ Start/Overview/GarbageCollect/Program.cs
@@ -1,13 +1,6 @@
 ﻿// Exercise file for LinkedIn Learning Course .NET Programming with C# by Joe Marini
 // Demonstration of Garbage Collection
 
-void DoSomeBigOperation() {
-    // create a large memory allocation that's only used in this function
-    byte[] myArray = new byte[1000000];
-
-    Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
-    Console.ReadLine();
-}
 
 // Retrieve and print the total memory allocated
 Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
@@ -15,8 +8,19 @@ Console.ReadLine();
 
 // Call the function that allocates a large memory chunk
 DoSomeBigOperation();
+
 // TODO: After the function completes, force a Garbage Collection 
+GC.Collect();
 
 // Retrieve and print the updated total memory amount
 Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
 Console.ReadLine();
+
+void DoSomeBigOperation()
+{
+    // create a large memory allocation that's only used in this function
+    byte[] myArray = new byte[1000000];
+
+    Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+    Console.ReadLine();
+}
